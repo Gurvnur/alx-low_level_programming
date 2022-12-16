28d65b60a1c2c9d4d4397ac6d7619dd9842336be
Victor Ataekong <victorataekong03@gmail.com>
2022-12-15 18:19:22 -0800

    1 main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
 
@@ -14,10 +15,10 @@ int main(void)
 	i = 0;
 
 /*
- * while (i = 10)
+ * while (i < 10)
  * {
  * Infinite loop - no increase of variable
- * putchar(1);
+ * putchar(i);
  * }
  */
 	printf("Infinite loop avoided! \\o/\n");
