d24d65124fd59cc2c32ee9ac2f9db672f68b57b3
Victor Ataekong <victorataekong03@gmail.com>
2022-12-23 11:26:12 -0800

    Capitalize string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,26 +2,26 @@
 
 /**
  * cap_string - capitalize all words of a string
- * @str: the string to be capitalized
+ * @x: the string to be capitalized
  *
  * Return: a pointer to the changed string
  */
-char *cap_string(char *str)
+char *cap_string(char *x)
 {
 	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 	int len = 13;
 	int a = 0, i;
 
-	while (str[a])
+	while (x[a])
 	{
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || str[a - 1]) && (str[a] >= 97 && str[a] <= 122))
-				str[a] = str[a] - 32;
+			if ((a == 0 || x[a - 1]) && (x[a] >= 97 && str[a] <= 122))
+				x[a] = x[a] - 32;
 			i++;
 		}
 		a++;
 	}
-	return (str);
+	return (x);
 }
