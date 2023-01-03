fee76528071956edb60ed451292095bd81efd265
Victor Ataekong <victorataekong03@gmail.com>
2023-01-02 17:22:05 -0800

    Memory set

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,13 @@
 #ifndef "MAIN_H"
 #define "MAIN_H"
+
+/**
+ * File: main.h
+ * Auth: all
+ * description: header file containing prototypes for all functions
+ * used
+ */
+
 #include <stddef.h>
 
 int _putchar(char c);
